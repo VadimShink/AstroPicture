4027c030087150a2f117d9e304e400b9cff909c6
Rigel <vadim_shink@icloud.com>
2022-09-04 15:17:47 +0300

    Initial Commit

---
+++ AstroPicture/AstroPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroPictureApp.swift
+//  AstroPicture
+//
+//  Created by Vadim Shinkarenko on 04.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AstroPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
